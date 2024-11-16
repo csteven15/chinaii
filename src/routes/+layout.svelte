b0e99fb7b15b722f3c5806e775baf709bad4c5ec
Steven Chen <chensteven8899@gmail.com>
2024-11-15 21:23:39 -0500

    Add phone number

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -103,6 +103,31 @@
     </div>
   </div>
 </nav>
+<div id="sticky-banner" class="flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+  <div class="flex items-center mx-auto">
+      <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+          <a href="tel:4076888998">
+            <button>
+              <span class="inline-flex p-1 me-3 bg-white rounded-full w-6 h-6 items-center justify-center flex-shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 text-slate-600">
+                  <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Phone</span>
+              </span> 
+            </button>
+          </a>
+          <span class="text-lg text-white">Call us now to place a pick up order: <a href="tel:4076888998" class="inline no-underline font-bold text-blue-600 underline dark:text-blue-500 underline-offset-2 decoration-600 dark:decoration-500 decoration-solid hover:no-underline">(407) 688-8998</a> or <a href="tel:4076888997" class="inline no-underline font-bold text-blue-600 underline dark:text-blue-500 underline-offset-2 decoration-600 dark:decoration-500 decoration-solid hover:no-underline">(407) 688-8997</a></span>
+      </p>
+  </div>
+  <div class="flex items-center">
+      <button data-dismiss-target="#sticky-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+          </svg>
+          <span class="sr-only">X</span>
+      </button>
+  </div>
+</div>
 <slot />
 <!-- Footer -->
 <div class="grid grid-cols-2 mx-auto max-w px-2 bg-gray-800 text-white">
